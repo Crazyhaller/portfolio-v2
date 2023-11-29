62652e9545b569c2e1f6ac0eda8d94784f9da22f
crazyhaller <suvigyamadrid@gmail.com>
2023-11-29 20:30:47 +0530

    add intro section

---
+++ components/intro.tsx
@@ -0,0 +1,113 @@
+'use client'
+
+import React from 'react'
+import Image from 'next/image'
+import photo from '@/public/photo.png'
+import { motion } from 'framer-motion'
+import Link from 'next/link'
+import { BsArrowRight, BsLinkedin } from 'react-icons/bs'
+import { HiDownload } from 'react-icons/hi'
+import { FaGithubSquare } from 'react-icons/fa'
+
+export default function Intro() {
+  return (
+    <section className="mb-28 max-w-[50rem] text-center sm:mb-0">
+      <div className="flex items-center justify-center">
+        <div className="relative">
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: 'tween',
+              duration: 0.2,
+            }}
+            className="relative"
+          >
+            <Image
+              src={photo}
+              alt="My photo"
+              width={192}
+              height={192}
+              quality={100}
+              priority={true}
+              className="h-28 w-28 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
+            />
+          </motion.div>
+          <motion.span
+            className="absolute bottom-0 right-0  text-3xl"
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: 'spring',
+              stiffness: 125,
+              delay: 0.1,
+              duration: 0.7,
+            }}
+          >
+            👋
+          </motion.span>
+        </div>
+      </div>
+
+      <motion.p
+        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-3xl"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          type: 'spring',
+          stiffness: 125,
+          delay: 0.1,
+          duration: 0.5,
+        }}
+      >
+        <span className="font-bold">Hello, I'm Suvigya Mishra.</span> I'm a{' '}
+        <span className="font-bold">Full-Stack Web Developer</span> with{' '}
+        <span className="font-bold">1+ years</span> of experience. I enjoy
+        building <span className="italic">websites & web apps</span>. My focus
+        is <span className="underline">MERN Stack | Next.js | Tailwind</span>
+      </motion.p>
+
+      <motion.div
+        className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 font-medium"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.1,
+        }}
+      >
+        <Link
+          href="#contact"
+          className="group bg-gray-900 text-white px-5 py-5 flex items-center gap-2 rounded-full outline-none focus:scale-105 hover:scale-105 hover:bg-gray-950 active:scale-95 transition"
+        >
+          Contact Me{' '}
+          <BsArrowRight className="group-hover:translate-x-1 transition" />
+        </Link>
+
+        <a
+          className="group bg-white px-5 py-5 flex items-center gap-2 rounded-full outline-none focus:scale-105 hover:scale-105 active:scale-95 transition cursor-pointer border border-black/10"
+          href="/CV.pdf"
+          download
+        >
+          Download CV{' '}
+          <HiDownload className="group-hover:translate-y-1 transition" />
+        </a>
+
+        <a
+          className="bg-white p-4 text-slate-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer border border-black/10 dark:bg-white/10 dark:text-white/60"
+          href="https://www.linkedin.com/in/suvigyamishra"
+          target="_blank"
+        >
+          <BsLinkedin />
+        </a>
+
+        <a
+          className="bg-white p-4 text-slate-700 flex items-center gap-2 text-[1.15rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/10 dark:bg-white/10 dark:text-white/60"
+          href="https://github.com/Crazyhaller"
+          target="_blank"
+        >
+          <FaGithubSquare />
+        </a>
+      </motion.div>
+    </section>
+  )
+}
