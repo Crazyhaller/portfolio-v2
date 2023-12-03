575011202f064065bf9cfce3fa26092ced3e0377
crazyhaller <suvigyamadrid@gmail.com>
2023-12-03 18:14:32 +0530

    add skills

--- components/project.tsx
+++ components/project.tsx
@@ -20,13 +20,9 @@ export default function Project({
     offset: ['0 1', '1.33 1'],
   })
 
-  const scaleProgess = useTransform(scrollYProgress, [0, 1], [0.6, 1])
+  const scaleProgess = useTransform(scrollYProgress, [0, 1], [0.8, 1])
 
-  const opacityProgess = useTransform(
-    scrollYProgress,
-    [0, 1],
-    [0.4, 1]
-  )
+  const opacityProgess = useTransform(scrollYProgress, [0, 1], [0.6, 1])
 
   return (
     <motion.div
