690605017f326026812a5c9e2379aac71d6393c5
crazyhaller <suvigyamadrid@gmail.com>
2023-12-05 19:09:54 +0530

    edit about section

--- components/about.tsx
+++ components/about.tsx
@@ -20,30 +20,34 @@ export default function About() {
       <SectionHeading>About Me</SectionHeading>
 
       <p className="mb-3">
-        After graduating with a degree in{' '}
-        <span className="font-medium">Accounting</span>, I decided to pursue my
-        passion for programming. I enrolled in a coding bootcamp and learned{' '}
-        <span className="font-medium">full-stack web development</span>.{' '}
+        While currently pursuing BTech degree in{' '}
+        <span className="font-medium">IT</span>, I decided to start my journey
+        for programming / development. I enrolled in an online course and
+        learned <span className="font-medium">Full-Stack web development</span>.{' '}
         <span className="italic">My favorite part of programming</span> is the
-        problem-solving aspect. I <span className="underline">love</span> the
-        feeling of finally figuring out a solution to a problem. My core stack
-        is{' '}
+        designing and building projcts. I{' '}
+        <span className="underline">love</span> the feeling of finally
+        completing the project or solving errors and bugs during the process. My
+        core stack is{' '}
         <span className="font-medium">
-          React, Next.js, Node.js, and MongoDB
+          React, Next.js, Tailwind, Node.js, and MongoDB
         </span>
-        . I am also familiar with TypeScript and Prisma. I am always looking to
-        learn new technologies. I am currently looking for a{' '}
-        <span className="font-medium">full-time position</span> as a software
-        developer.
+        . I am also familiar with TypeScript and the programming language Java.
+        I am always looking to learn new technologies. I am currently looking
+        for a <span className="font-medium">full-time position</span> as a Full
+        Stack or Frontend Developer.
       </p>
 
       <p>
         <span className="italic">When I'm not coding</span>, I enjoy playing
-        video games, watching movies, and playing with my dog. I also enjoy{' '}
-        <span className="font-medium">learning new things</span>. I am currently
-        learning about{' '}
-        <span className="font-medium">history and philosophy</span>. I'm also
-        learning how to play the guitar.
+        football, listening to music, play video games and watch YouTube. I also
+        enjoy{' '}
+        <span className="font-medium">
+          listening to podcasts, read articles and stuff and watch doumentaries
+          about different things
+        </span>
+        . I also want to learn mountain climbing and swimming in the future as I
+        am very fond of nature, mountains and water.
       </p>
     </motion.section>
   )
