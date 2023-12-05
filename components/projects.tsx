575a4350f74a090362dc8a80713f024508ad4eba
crazyhaller <suvigyamadrid@gmail.com>
2023-12-05 19:14:13 +0530

    edit projects section activesectionview

--- components/projects.tsx
+++ components/projects.tsx
@@ -4,10 +4,18 @@ import React, { useEffect } from 'react'
 import SectionHeading from './sectionHeading'
 import { projectsData } from '@/lib/data'
 import Project from './project'
-import { useSectionInView } from '@/lib/hooks'
+import { useActiveSectionContext } from '@/context/activeSectionContext'
+import { useInView } from 'react-intersection-observer'
 
 export default function Projects() {
-  const { ref } = useSectionInView('Projects', 0.5)
+  const { ref, inView } = useInView()
+  const { setActiveSection, timeOfLastClick } = useActiveSectionContext()
+
+  useEffect(() => {
+    if (inView && Date.now() - timeOfLastClick > 1000) {
+      setActiveSection('Projects')
+    }
+  }, [inView, setActiveSection, timeOfLastClick])
 
   return (
     <section ref={ref} id="projects" className="scroll-mt-28 mb-28">
